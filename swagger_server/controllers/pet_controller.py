2783258fc364c790f525077f94bc502d8e35a152
lfdantoni <lfdantoni@gmail.com>
2018-04-30 19:47:20 -0300

    sss

--- swagger_server/controllers/pet_controller.py
+++ swagger_server/controllers/pet_controller.py
@@ -72,7 +72,7 @@ def get_pet_by_id(petId):  # noqa: E501
 
     :rtype: Pet
     """
-    return 'do some magic! puto el que lee test6'
+    return 'do some magic! puto el que lee test7'
 
 
 def update_pet(body):  # noqa: E501
